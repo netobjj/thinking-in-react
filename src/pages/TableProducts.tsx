384bc74c880c4a13333bf5351944ca8d01da9fb6
netobjj <joseeuclides18@gmail.com>
2025-07-16 18:31:52 -0300

    add search

--- src/pages/TableProducts.tsx
+++ src/pages/TableProducts.tsx
@@ -1,16 +1,66 @@
 import './TableProducts.css';
 import { Product, ProductsDatabase } from '../db/data';
+import { useState } from 'react';
 
-function SearchBar() {
 
+function TableProducts() {
+    const [filterText, setFilterText] = useState<string>("");
+    const [inStockOnly, setInStockOnly] = useState<boolean>(false);
+
+    function onFilterTextChange(newText: string) {
+        setFilterText(newText)
+    }
+
+    function onInStockOnlyChange(newValue: boolean) {
+        setInStockOnly(newValue);
+    }
+
+    const products = ProductsDatabase;
+
+    return (
+        <div className="table-products">
+            <div className="col">
+                <div className='row'>
+                    <SearchBar
+                        filterText={filterText}
+                        inStockOnly={inStockOnly}
+                        onFilterTextChange={onFilterTextChange}
+                        onInStockOnlyChange={onInStockOnlyChange}
+                    />
+
+                </div>
+                <div className='row'>
+                    <ProductTable
+                        products={products}
+                        filterText={filterText}
+                        inStockOnly={inStockOnly}
+                    />
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+
+function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }:
+    { filterText: string, inStockOnly: boolean, onFilterTextChange: Function, onInStockOnlyChange: Function }) {
     return (
         <form>
             <div className="row mt-5">
-                <input type="text" placeholder='search' />
+                <input type="text"
+                    placeholder='search'
+                    value={filterText}
+                    onChange={(e) => onFilterTextChange(e.target.value)}
+                />
             </div>
             <div className="row mt-3">
                 <label>
-                    <input type="checkbox" />
+                    <input
+                        type="checkbox"
+                        checked={inStockOnly}
+                        onChange={(e) => onInStockOnlyChange(e.target.checked)}
+                    />
 
                     Only show products in stock!
                 </label>
@@ -35,15 +85,22 @@ function ProductRow({ category, products }: { category: string, products: Produc
     return productsFiltered;
 } */
 
-function ProductCategoryRow({ category, products }: { category: string, products: Product[] }) {
+function ProductCategoryRow({ category, products, filterText, inStockOnly }: { category: string, products: Product[], filterText: string, inStockOnly: boolean }) {
     const productsFiltered = products
         .filter(c => c.category === category)
-        .map(p => (
-            <tr>
-                <td>{p.stocked === false ? <span className='red'>{p.name}</span> : p.name}</td>
-                <td>{p.price}</td>
-            </tr>
-        ));
+        .map(p => {
+
+            if (filterText.trim() !== "" && p.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) return;
+            if (inStockOnly && !p.stocked) return;
+
+
+            return (
+                <tr>
+                    <td>{p.stocked === false ? <span className='red'>{p.name}</span> : p.name}</td>
+                    <td>{p.price}</td>
+                </tr>
+            )
+        });
 
     return (
         <>
@@ -57,7 +114,7 @@ function ProductCategoryRow({ category, products }: { category: string, products
     );
 }
 
-function ProductTable({ products }: { products: Product[] }) {
+function ProductTable({ products, filterText, inStockOnly }: { products: Product[], filterText: string, inStockOnly: boolean }) {
     const uniquesCategories: string[] = []
     products.forEach((v, i, arr) => !uniquesCategories.includes(v.category) ? uniquesCategories.push(v.category) : "");
 
@@ -72,7 +129,12 @@ function ProductTable({ products }: { products: Product[] }) {
             </thead>
             <tbody>
                 {uniquesCategories.map(c => {
-                    return <ProductCategoryRow category={c} products={products} />
+                    return <ProductCategoryRow
+                        category={c}
+                        products={products}
+                        filterText={filterText}
+                        inStockOnly={inStockOnly}
+                    />
                 })
                 }
             </tbody>
@@ -81,19 +143,5 @@ function ProductTable({ products }: { products: Product[] }) {
 }
 
 
-
-function TableProducts() {
-    const products = ProductsDatabase;
-
-    return (
-        <div className="table-products">
-            <div className="col">
-                <div className='row'><SearchBar /></div>
-                <div className='row'><ProductTable products={products} /></div>
-            </div>
-
-        </div>
-    );
-}
 
 export default TableProducts;
